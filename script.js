d8c86d3392cf0cf7524019f6b8e2ca7364c20ed8
Lakshita-Kumawat <155381670+Lakshita-Kumawat@users.noreply.github.com>
2024-07-28 15:54:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ div.innerHTML=`<img src="images/cricketer2.png" alt="trophy" loading="eager">
 <img src="images/cricketer4.png" alt="trophy" loading="eager">`
 
 document.querySelector('header').append(div);
+
+const div2 = document.createElement('div');
+div2.setAttribute('id','snow');
+
+div2.innerHTML = `<span style="--i:1"></span>
+<span style="--i:2;"></span>
+<span style="--i:4;"></span>
+<span style="--i:1;"></span>
+<span style="--i:3;"></span>
+<span style="--i:2;"></span>`;
+
+document.querySelector('body').prepend(div2);
