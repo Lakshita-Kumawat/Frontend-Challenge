3e1d056aa5d75a68bfb31ca19b68c9915dcdaf83
Lakshita-Kumawat <155381670+Lakshita-Kumawat@users.noreply.github.com>
2024-07-28 14:46:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const headerH1 = document.querySelector('header h1');
-const headerP = document.querySelector('header p');
-
-window.addEventListener('scroll', ()=>{
-    let value = window.scrollY;
-    console.log(value);
-    headerH1.style.right = value * 0.5+'px';
-    headerP.style.left = value * 0.5+'px';
-})
-
 const div = document.createElement('div');
 div.innerHTML=`<img src="images/cricketer2.png" alt="trophy" loading="eager">
 <img src="images/cricketer3.png" alt="trophy" loading="eager">
