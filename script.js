6ac7a49221a39b511f3ca9c51ea39a9e430d9b1b
Lakshita-Kumawat <155381670+Lakshita-Kumawat@users.noreply.github.com>
2024-07-31 14:37:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ div2.innerHTML = `<span style="--i:1"></span>
 <span style="--i:2;"></span>`;
 
 document.querySelector('body').prepend(div2);
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("header h1, header p",{
+    y:50,
+    duration:1,
+});
+
+gsap.to("header img",{
+    y:20,
+    duration:1,
+    opacity:1
+})
+
+let tl = gsap.timeline({scrollTrigger:{
+    trigger:'#about h2, #about p',
+    markers:true,
+    start:'top 80%',
+    end:'+=1200',
+    toggleAction:'play pause resume restart',
+    scrub:3,
+}});
+
+tl.from('#about h2, #about p, #about img',{
+    x:50,
+    duration:2,
+})
+
+tl.from('#join h2, #join p',{
+    y:20,
+    duration:2,
+})
+
+tl.from('#fee h2, #fee p',{
+    y:20,
+    duration:2,
+})
+
+tl.from('#location h2, #location p, #location img',{
+    x:-50,
+    duration:3,
+})
+
+tl.from('#schedule ul',{
+    x:50,
+    duration:2,
+})
